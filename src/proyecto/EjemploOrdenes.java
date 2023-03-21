53e4f71d5721c82a63577136144e242d1063c728
Ana Verduguez <122890538+AVerduguez-AV@users.noreply.github.com>
2023-03-21 11:23:10 -0300

    Update EjemploOrdenes.java

--- src/proyecto/EjemploOrdenes.java
+++ src/proyecto/EjemploOrdenes.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class EjemploOrdenes {
 	public static void main(String[] args) {
     	
-    //Creo 3 objetos o instancias para la clase cliente
+    	//Creo 3 objetos o instancias para la clase cliente
 	Cliente cliente1 = new Cliente("Romina", "Martinez");
 	Cliente cliente2 = new Cliente("Julieta", "Lopez");
 	Cliente cliente3 = new Cliente("Ana", "Diaz");
@@ -28,50 +28,50 @@ public class EjemploOrdenes {
 	
 	//Creo 3 ordenes con diferentes clientes, fechas y productos 
 	OrdenCompra orden1 = new OrdenCompra("Orden 1");
-    orden1.setCliente(cliente1);
-    orden1.setFecha(new Date());
-    orden1.addProducto(producto1);
-    orden1.addProducto(producto2);
-    orden1.addProducto(producto3);
-    orden1.addProducto(producto4);
+	orden1.setCliente(cliente1);
+	orden1.setFecha(new Date());
+	orden1.addProducto(producto1);
+	orden1.addProducto(producto2);
+	orden1.addProducto(producto3);
+	orden1.addProducto(producto4);
  
-    OrdenCompra orden2 = new OrdenCompra("Orden 2");
-    orden2.setCliente(cliente2);
-    orden2.setFecha(new Date());
-    orden2.addProducto(producto5);
-    orden2.addProducto(producto6);
-    orden2.addProducto(producto7);
-    orden2.addProducto(producto8);
+        OrdenCompra orden2 = new OrdenCompra("Orden 2");
+        orden2.setCliente(cliente2);
+        orden2.setFecha(new Date());
+        orden2.addProducto(producto5);
+        orden2.addProducto(producto6);
+        orden2.addProducto(producto7);
+        orden2.addProducto(producto8);
  
     
-    OrdenCompra orden3 = new OrdenCompra("Orden 3");
-    orden3.setCliente(cliente3);
-    orden3.setFecha(new Date());
-    orden3.addProducto(producto9);
-    orden3.addProducto(producto10);
-    orden3.addProducto(producto11);
-    orden3.addProducto(producto12);
+        OrdenCompra orden3 = new OrdenCompra("Orden 3");
+        orden3.setCliente(cliente3);
+        orden3.setFecha(new Date());
+        orden3.addProducto(producto9);
+        orden3.addProducto(producto10);
+        orden3.addProducto(producto11);
+        orden3.addProducto(producto12);
     
-    //Imprimo todos los valores de cada orden pasando como parametro la descripcion
-    imprimir(orden1);
-    imprimir(orden2);
-    imprimir(orden3);
+        //Imprimo todos los valores de cada orden pasando como parametro la descripcion
+        imprimir(orden1);
+        imprimir(orden2);
+        imprimir(orden3);
    
 	}
 	
-    //Creo un metodo que me permitira imprimir por pantalla
-    public static void imprimir(OrdenCompra orden) {
-	    System.out.println("Orden N°: " + orden.getDescripcion());
-	    System.out.println("Fecha: " + orden.getFecha());
-	    System.out.println("Cliente: " + orden.getCliente().getNombre() + " " + orden.getCliente().getApellido());
-	    System.out.println("Productos:");
-	    for (Producto producto : orden.getProductos()) {
-	    	if (producto != null) {
-	    		System.out.println("-" + producto.getFabricante() + " " + producto.getNombre() + " $" + producto.getPrecio());
-	        }
-        }
-	    System.out.println("El total es: $" + orden.getTotalProductos());
-	    System.out.println("-------------------------------------------");
+        //Creo un metodo que me permitira imprimir por pantalla
+        public static void imprimir(OrdenCompra orden) {
+		System.out.println("Orden N°: " + orden.getDescripcion());
+	        System.out.println("Fecha: " + orden.getFecha());
+	        System.out.println("Cliente: " + orden.getCliente().getNombre() + " " + orden.getCliente().getApellido());
+	        System.out.println("Productos:");
+	        for (Producto producto : orden.getProductos()) {
+	    		if (producto != null) {
+	    			System.out.println("-" + producto.getFabricante() + " " + producto.getNombre() + " $" + producto.getPrecio());
+	       		 }
+        	}
+	    	System.out.println("El total es: $" + orden.getTotalProductos());
+	    	System.out.println("-------------------------------------------");
    }
 }
 
